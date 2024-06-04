81f80feacf72b2b0f0f82cf0604df674173c3133
nrzdhmz <nuruzadahamza@gmail.com>
2024-06-05 00:17:01 +0400

    ui update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="phone">
+        SLAM
+      </div>
+      <div className="phone"></div>
+    </div>      
+  )
+}
+
+export default App
